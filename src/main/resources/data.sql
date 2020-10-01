d418e10a1a782d984b6bf192b7f2b32ff5186bb5
sylvain amianto <samianto@gmail.com>
2020-10-01 12:22:11 +0200

    init api

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,4 +4,5 @@ insert into collegue(matricule, nom, prenom, email,date_naissance,photo_url) val
 insert into collegue(matricule, nom, prenom, email,date_naissance,photo_url) values ('qsd987654321', 'Gelaron', 'Delphine', 'qsd@cqlTeam.go'     ,'1990-04-26','https://www.colocappart.ch/wp-content/uploads/2019/07/noimg1.jpg');
 insert into collegue(matricule, nom, prenom, email,date_naissance,photo_url) values ('wxc231564897', 'Dhalor' , 'Homer'   , 'wxc@cqlTeam.go'     ,'1953-07-17','https://www.colocappart.ch/wp-content/uploads/2019/07/noimg1.jpg');
 insert into collegue(matricule, nom, prenom, email,date_naissance,photo_url) values ('ert879546213', 'kroche' , 'Sarah'   , 'ert@cqlTeam.go'     ,'1976-03-10','https://www.colocappart.ch/wp-content/uploads/2019/07/noimg1.jpg');
+insert into collegue(matricule, nom, prenom, email,date_naissance,photo_url) values ('cql1405', 'Amianto' , 'Julie'   , 'ert@cqlTeam.go'     ,'1983-05-14','https://www.colocappart.ch/wp-content/uploads/2019/07/noimg1.jpg');
 
